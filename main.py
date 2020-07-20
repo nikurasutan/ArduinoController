65d8c8a9b09b108dc64c5d29cef797ac2ac5db97
Nikurasukun <55510407+Nikurasukun@users.noreply.github.com>
2020-07-20 08:47:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,21 +12,26 @@ app.config['SECRET_KEY'] = '85e27c84f9ce488e85a383771f2a1c74' # Secret Key um ge
 def home(): # Diese Funktion wird ausgeführt wenn diese Route angesteuert wird
     global arduinoData, allright # Globale Variabeln festlegen
     form = AuswalFormular() # Das Formular auswählen, das angezeigt werden soll
-    if form.validate_on_submit(): # wenn das Formular bei einer Post-Anfrage korrekt ist tu das
-        if form.aktion.data == 'verbinden': # Wenn Verbinden ausgewählt
-            arduinoData = serial.Serial(form.serialDevice.data, 9600) # Setze die Eckdaten der Seriellen Verbindung fest
-            allright = True # Da Funktion valide
-        elif form.aktion.data != 'verbinden' and arduinoData is None: # Wenn etwas anderes als verbinden ausgewählt wurde und die Eckdaten der Verbindung noch nicht festgelegt sind.
-            allright = False # Nicht valide Funktion
-        elif form.aktion.data == 'dreh' and arduinoData is not None: # Wenn dreh ausgewählt und Eckdaten der Verbindung sind eingetragen
-            drehen(arduinoData) # sende drehen Signal an den Arduino
-            allright = True # valide Funktion
+    if form.validate_on_submit():# wenn das Formular bei einer Post-Anfrage korrekt ist tu das
+        if form.serialDevice.data is not "":
+            if form.aktion.data == 'verbinden': # Wenn Verbinden ausgewählt
+                arduinoData = serial.Serial(form.serialDevice.data, 9600) # Setze die Eckdaten der Seriellen Verbindung fest
+                allright = True # Da Funktion valide
+            elif form.aktion.data != 'verbinden' and arduinoData is None: # Wenn etwas anderes als verbinden ausgewählt wurde und die Eckdaten der Verbindung noch nicht festgelegt sind.
+                allright = False # Nicht valide Funktion
+            elif form.aktion.data == 'dreh' and arduinoData is not None: # Wenn dreh ausgewählt und Eckdaten der Verbindung sind eingetragen
+                drehen(arduinoData) # sende drehen Signal an den Arduino
+                allright = True # valide Funktion
 
-        if allright:
-            flash(f'Es wurde das Signal {form.aktion.data} an den Arduino geschickt!', 'success') # Wenn Funktion valide
-        else: # wenn nicht valide
+            if allright:
+                flash(f'Es wurde das Signal {form.aktion.data} an den Arduino geschickt!', 'success') # Wenn Funktion valide
+            else: # wenn nicht valide
+                flash(
+                    'Den Arduino ist noch nicht verbunden, bitte erst mittels der Funktion Verbinden den Arduino mit dem Server verbinden.', 'warning') # sende eine Warnung
+        else:
             flash(
-                'Den Arduino ist noch nicht verbunden, bitte erst mittels der Funktion Verbinden den Arduino mit dem Server verbinden.', 'warning') # sende eine Warnung
+                'Bitte einen Wert für den seriellen Port angeben',
+                'warning')  # sende eine Warnung
 
     return render_template('home.html', form=form) # Render das Template Home
 
